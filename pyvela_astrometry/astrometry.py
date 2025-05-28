9b3ab6ec1baec52ab197b8d3ae090fac323f69f5
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2025-05-28 21:36:25 +0200

    parallax

--- pyvela_astrometry/astrometry.py
+++ pyvela_astrometry/astrometry.py
@@ -85,7 +85,7 @@ class AstrometryData:
                     ra_errs.append(float(line.split()[2]))
                     decs.append(line.split()[3])
                     dec_errs.append(float(line.split()[4]))
-        mjds = Time(mjds, format="mjd")
+        mjds = Time(mjds, format="decimalyear")
         posns = SkyCoord(ras, decs, unit=("hour", "deg"), frame="icrs", obstime=mjds)
         # convert to proper arcsec
         ra_errs = np.array(ra_errs) * np.cos(posns.dec) * 15 * u.arcsec
@@ -123,20 +123,21 @@ def sky_position(
     x0 = np.array((cosα0 * cosδ0, sinα0 * cosδ0, sinδ0))
 
     xdot_ra = np.array((-sinα0, cosα0, 0)) * pmra
-    xdot_dec = (-cosα0 * sinδ0, -sinα0 * sinδ0, cosδ0) * pmdec
+    xdot_dec = np.array((-cosα0 * sinδ0, -sinα0 * sinδ0, cosδ0)) * pmdec
     delta_x_pm = (xdot_ra + xdot_dec) * dt
 
     x1 = x0 + delta_x_pm.to_value(u.dimensionless_unscaled, equivalencies=u.dimensionless_angles())
     x1_mag = np.sqrt(np.dot(x1, x1))
     x1hat = x1 / x1_mag
 
-    D = (u.AU / px).to(u.km, equivalencies=u.dimensionless_angles())
-    r = ssb_earth_pos
-    delta_x_px = (r - np.dot(r, x1hat) * x1hat) / D
-
-    x2 = x1hat + delta_x_px
-    x2_mag = np.sqrt(np.dot(x2, x2))
-    x2hat = x2 / x2_mag
+    if px > 0:
+        D = (u.AU / px).to(u.km, equivalencies=u.dimensionless_angles())
+        r = ssb_earth_pos
+        x2 = x1hat - r / D
+        x2mag = np.sqrt(np.dot(x2, x2))
+        x2hat = x2 / x2mag
+    else:
+        x2hat = x1hat
 
     dec = np.arcsin(x2hat[2])
     ra = np.arctan2(x2hat[1], x2hat[0])
@@ -170,13 +171,18 @@ def get_lnlike(data: AstrometryData):
         pmra = params[2] * (u.mas / u.year)
         pmdec = params[3] * (u.mas / u.year)
         px = params[4] * u.mas
+        raefac = params[5]
+        decefac = params[6]
 
         ra, dec = sky_positions(data.dt, ra0, dec0, pmra, pmdec, px, data.ssb_earth_pos)
 
-        chi2 = np.sum(((data.ra - ra) / data.ra_errs) ** 2) + np.sum(
-            ((data.dec - dec) / data.dec_errs) ** 2
+        ra_errs = data.ra_errs * raefac
+        dec_errs = data.dec_errs * decefac
+
+        chi2 = np.sum(((data.ra - ra) / ra_errs) ** 2) + np.sum(
+            ((data.dec - dec) / dec_errs) ** 2
         )
-        norm = np.sum(np.log((data.ra_errs * data.dec_errs).value))
+        norm = np.sum(np.log((ra_errs * dec_errs).value))
 
         return -0.5 * chi2 - norm
 
